d80775d92ccd51451f778cd818053e1744980885
Garoon Arkian <garoonearkian@gmail.com>
2026-08-30 12:08:58 +0330

    add power to utils and greeting del from main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 print("Hello World!")
 print("this is a test")
 
-def greeting():
-    return "hello"
 
 def squer(a):
     return a ** 2
--- utils.py
+++ utils.py
@@ -22,4 +22,7 @@ def div(a, b):
     """
     return a / b
 
+def power(a, b)
+    return a ** b
+
      
