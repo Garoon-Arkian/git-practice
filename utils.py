a7a16f318675eaba4dfc9d0fc0e4a325ee12461b
Garoon Arkian <garoonearkian@gmail.com>
2026-08-30 11:29:50 +0330

    add docksting to code

--- utils.py
+++ utils.py
@@ -1,13 +1,25 @@
 def add(a, b):
+    """
+    add two numbers
+    """
     return a + b
 
 def sub(a, b):
+    """
+    subtrcts two numbers
+    """
     return a - b
 
 def mul(a, b):
+    """
+    mutiply two numbers
+    """
     return a * b
 
 def div(a, b):
+    """
+    divide two numbers
+    """
     return a / b
 
      
